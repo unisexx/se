75ee09f1e300a1b460d73e637547e9f19727a0d6
unisexx <unisexx@gmail.com>
2016-01-05 22:24:36 +0700

    ....

--- autopost/config.php
+++ autopost/config.php
@@ -2,6 +2,6 @@
 $_config['dbdriver'] = 'mysql';
 $_config['server'] = 'localhost';
 $_config['username'] = 'kpoplove_se';
-$_config['password'] = 'TG4Bf4CZ';
+$_config['password'] = 'ISkQBcWTm7';
 $_config['database'] = 'kpoplove_se';
 ?>
