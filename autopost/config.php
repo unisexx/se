a00a2f7339963781692d2049dc344711ac6f212b
unisexx <unisexx@gmail.com>
2016-03-10 01:56:53 +0700

    ....

--- autopost/config.php
+++ autopost/config.php
@@ -1,7 +1,7 @@
 <?php
 $_config['dbdriver'] = 'mysql';
 $_config['server'] = 'localhost';
-$_config['username'] = 'kpoplove_se';
-$_config['password'] = 'ISkQBcWTm7';
-$_config['database'] = 'kpoplove_se';
+$_config['username'] = 'addfrien_kpop';
+$_config['password'] = 'RZSAQiJme9';
+$_config['database'] = 'addfrien_kpop';
 ?>
